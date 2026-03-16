829726192b75f8d45bf3efc65826a5894d32ae82
cauelaurentino <cauedasilvalaurentino@gmail.com>
2026-03-16 11:28:29 -0300

    service atualizado

--- src/main/java/caue/restAPI/model/Produto.java
+++ src/main/java/caue/restAPI/model/Produto.java
@@ -29,5 +29,55 @@ public class Produto {
     private String descricao;
     private Integer quantidade;
     
-    //teste
+    //GETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+   // SETTERS
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(Double preco) {
+        if (preco < 0) {
+        throw new IllegalArgumentException("Preço não pode ser negativo");
+        }
+        this.preco = preco;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+   
+   
+    
+    
 }
